b968d16be3903c6ad26ccafd82dfe432e809d41d
debabratabar <debabrata.bar1999@gmail.com>
2025-11-29 15:17:39 +0530

    Sum-of-numbers-in-string

---
+++ Just-One-Mismatch.py
@@ -0,0 +1,31 @@
+# /*
+
+# Question Link : https://www.geeksforgeeks.org/problems/just-one-mismatch1714/1
+
+
+# */
+
+class Solution:
+    def isStringExist (self, arr, N, S):
+        # code here 
+        
+        cnt = 0 
+        
+        for i in arr:
+            if len(S) == len(i):
+                for j in range(0,len(i)):
+                    if i[j] != S[j]:
+                        cnt+=1
+                        
+                if cnt==1:
+                    return True
+                else:
+                    cnt=0
+                        
+        
+        # print(cnt)                
+        
+        # if cnt==1:
+        #     return True 
+            
+        return False
