0d30a6d64405fe28f1390de5427f87da7b4c630c
debabratabar <debabrata.bar1999@gmail.com>
2025-11-29 15:15:32 +0530

    Sum-of-numbers-in-string

---
+++ Sum-of-numbers-in-string.py
@@ -0,0 +1,32 @@
+# /*
+
+# Question Link : https://www.geeksforgeeks.org/problems/sum-of-numbers-in-string-1587115621/1
+
+
+# */
+
+class Solution:
+    def findSum(self, s):
+        #code here
+        #code here
+        num = ''
+        # digit = 0
+        sum=0
+        
+        for i in s:
+            if i.isdigit():
+                num +=i
+            else:
+                if num !='':
+                    sum+=int(num)
+                    num=''
+    
+    
+        if num !='':
+            sum+=int(num)
+    
+        # print(int(num))
+        # print(sum)
+        
+        return sum  
+                    
